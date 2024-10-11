f3dddaa5a3839a09f4849b0ae75bd5ed091dacbc
Oktawia Miluch <oktawia.miluch@staburo.de>
2024-10-11 15:13:00 +0200

    utility files

---
+++ R/pertvimp-package.R
@@ -0,0 +1,7 @@
+#' @keywords internal
+"_PACKAGE"
+
+# TODO: put as @references link to the paper with the description of the method implemented in the package
+## usethis namespace: start
+## usethis namespace: end
+NULL
--- R/utils.R
+++
@@ -1,1 +0,0 @@
-utils::globalVariables(c("y", "p_adj_FDR", "p_adj_FWER", "p_unadj", "quantile_per_variable", "quantile_pooled", "varname"))
